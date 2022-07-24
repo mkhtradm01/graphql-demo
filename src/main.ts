b975c2124d14362bacd1286f82e4cd4cf3f268a4
Adamu Muktar <adamu.muktar@dev-thought.cool>
2022-07-24 13:00:37 +0600

    - Sets app GlobalPrefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const GLOBAL_PREFIX = 'api';
+  app.setGlobalPrefix(GLOBAL_PREFIX);
   await app.listen(3000);
 }
 bootstrap();
